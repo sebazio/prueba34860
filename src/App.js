ff55cda71e701ca4a50826f09cc14fc1d40616a5
sebazdev <szuviria@gmail.com>
2022-12-13 21:11:39 -0300

    cambio en nueva branch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hola chicos</h1>
       </header>
     </div>
   );
